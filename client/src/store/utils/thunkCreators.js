dff92147422c55753ceefb0644289b7b3064ea41
tyler <tyler_1701@hotmail.com>
2021-10-29 17:39:37 -0400

    syntax

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { gotUser, setFetchingStatus } from '../user';
 
 axios.interceptors.request.use(async function (config) {
-  const token = await localStorage.getItem('messenger-token');
+  const token = await localStorage.getItem('test-token');
   config.headers['x-access-token'] = token;
 
   return config;
@@ -25,7 +25,7 @@ export const fetchUser = () => async (dispatch) => {
 export const register = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post('/auth/register', credentials);
-    await localStorage.setItem('messenger-token', data.token);
+    await localStorage.setItem('test-token', data.token);
     dispatch(gotUser(data));
   } catch (error) {
     console.error(error);
@@ -36,7 +36,7 @@ export const register = (credentials) => async (dispatch) => {
 export const login = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post('/auth/login', credentials);
-    await localStorage.setItem('messenger-token', data.token);
+    await localStorage.setItem('test-token', data.token);
     dispatch(gotUser(data));
   } catch (error) {
     console.error(error);
@@ -47,7 +47,7 @@ export const login = (credentials) => async (dispatch) => {
 export const logout = (id) => async (dispatch) => {
   try {
     await axios.delete('/auth/logout');
-    await localStorage.removeItem('messenger-token');
+    await localStorage.removeItem('test-token');
     dispatch(gotUser({}));
   } catch (error) {
     console.error(error);
